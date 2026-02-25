1bd7e9864403e013099ace0b7c7fe8331cde6975
TopchetoEU <36534413+TopchetoEU@users.noreply.github.com>
2026-02-25 09:02:25 +0200

    add bundler

---
+++ lib/tal/bundle.lua
@@ -0,0 +1,91 @@
+local args = require "std.fmt.args";
+local mklua= require "tal.mklua"
+
+local help_msg = [[tal bundle by TopchetoEU
+
+Generates a C program, a bundle of a lua module + its dependencies, and optionally compiles it to an executable
+
+Flags:
+
+<entry> - the name of a lua module to be bundled
+
+--compile-cmd (-c) ... - specifies the rest of the arguments as a compiler, to be used to produce the executable.
+	The static libraries, used by the lua module will be appended to the arguments.
+	The contents of the generated C file will be piped in stdin
+--output (-o) <file> - specifies the file to which the C source should be written
+--bootstrap <module> - specifies an alternative to "tal.entry" that will bootstrap the compiled module
+]]
+
+return args.cli {
+	ctx = {},
+	flags = {
+		help = args.bool "help",
+		["compile-cmd"] = args.rest "compile_cmd",
+		output = args.str "output",
+		deps = args.bool "deps",
+		libs = args.bool "libs",
+		alibs = args.bool "alibs",
+
+		bootstrap = args.str "bootstrap",
+
+		c = "compile-cmd",
+		o = "output",
+	},
+	rest = args.str "entry",
+	next = function (ctx)
+		if ctx.help then
+			return io.stderr:write(help_msg);
+		end
+
+		if not ctx.entry then
+			return io.stderr:write "error: an entry must be specified\n";
+		end
+
+		local mklua_ctx = {
+			entries = { ctx.bootstrap or "tal.entry", ctx.entry },
+			args = { "-m", ctx.entry },
+			path = package.path,
+			cpath = package.cpath,
+			entry = true,
+			main = true,
+		};
+
+		if ctx.compile_cmd then
+			io.stderr:write "error: compile command is not supported\n";
+		elseif ctx.output then
+			local f, close = mklua.open_w(ctx.output);
+			mklua.gen(mklua_ctx, { f = f });
+			close(f);
+		elseif ctx.libs or ctx.alibs then
+			local libs = {};
+			local alibs = {};
+
+			mklua.gen(mklua_ctx, { libs = libs, alibs = alibs });
+
+			if ctx.libs then
+				for i = 1, #libs do
+					io.stdout:write(libs[i] .. "\n");
+				end
+			end
+
+			if ctx.alibs then
+				for i = 1, #alibs do
+					io.stdout:write(alibs[i] .. "\n");
+				end
+			end
+		elseif ctx.deps then
+			local deps = {};
+
+			mklua.gen(mklua_ctx, { deps = deps });
+
+			if ctx.deps then
+				for k, v in pairs(deps) do
+					io.stdout:write(v .. "\n");
+				end
+			end
+		else
+			io.stderr:write "--compile-cmd or --output must be specified\n";
+			return io.stderr:write(help_msg);
+		end
+	end
+}
---
+++ lib/tal/mklua.lua
@@ -0,0 +1,408 @@
+local args = require "std.fmt.args";
+local ffi = require "ffi";
+
+--- @class tal.mklua.ctx
+--- @field path? string
+--- @field cpath? string
+--- @field ffi_path? string
+--- @field help? boolean
+--- @field deps? boolean
+--- @field libs? boolean
+--- @field output? string | file*
+--- @field entries string[]
+--- @field args? string[]
+--- @field g? boolean
+--- @field main? boolean
+--- @field entry? boolean
+
+--- @class tal.mklua.out
+--- @field f? file*
+--- @field deps? table<string, string>
+--- @field ffi_deps? table<string, string>
+--- @field libs? string[]
+--- @field alibs? string[]
+
+local help_msg = [[
+A tool that walks a lua dependency tree and emits a C file, used to
+compile the application to a self-contained executable.
+
+Flags:
+
+<module[:output-name]> - exports the given module with a luaopen function.
+	If :output-name is specified, changes the luaopen to the specified module name
+
+--deps - outputs a list of files the entries depend on
+--libs - outputs a list of native libraries the entries depend on
+--makefile - generates a makefile
+
+--entry - generates a talb_entry function, which takes an initialized lua state
+	and executes the first entry
+--main - generates a main function, which creates a lua state and runs the entry
+	implies `--entry`
+--output=<file> (-o <file>) - the file to write to. May be stdout, piped into a compiler
+--debug (-g) - emits debugging data in the lua bytecode
+
+--path - specifies a lua path to search (separated by ;, ;; is a substitute for the interpreter's default)
+--cpath - specifies a c path to search
+--arg ... - passes the rest of the args to the last entry, if "--entry" is specified
+]];
+
+local entry_format = [[
+#ifndef TALB_ENTRY_NAME
+	#define TALB_ENTRY_NAME talb_entry
+#endif
+
+TALB_EXPORT int TALB_ENTRY_NAME(lua_State *ctx, int argc, const char **argv) {
+	int larg, lerrcb, i, ok;
+	lua_checkstack(ctx, argc + %d + 16);
+
+	/* Emit other requiref-s */
+	%s
+
+	lua_createtable(ctx, argc, 0);
+	larg = lua_gettop(ctx);
+
+	lua_pushvalue(ctx, larg);
+	lua_setglobal(ctx, "arg");
+
+	%s(ctx);
+	%s(ctx);
+
+	/* custom arguments */
+%s
+
+	for (i = 1; i < argc; i++) {
+		lua_pushstring(ctx, argv[i]);
+		lua_pushinteger(ctx, i);
+		lua_pushvalue(ctx, -2);
+		lua_settable(ctx, larg);
+	}
+
+	ok = !lua_pcall(ctx, argc + %d - 1, 0, lerrcb);
+	if (!ok) fprintf(stderr, "lua error: %%s\n", lua_tostring(ctx, -1));
+
+	return ok;
+}
+]];
+
+local main_format = [[
+#ifndef TALB_MAIN_NAME
+	#define TALB_MAIN_NAME main
+#endif
+
+TALB_EXPORT int TALB_MAIN_NAME(int argc, const char **argv) {
+	int ok;
+	lua_State *ctx = luaL_newstate();
+	luaL_openlibs(ctx);
+
+	ok = TALB_ENTRY_NAME(ctx, argc, argv);
+	lua_close(ctx);
+	return ok ? 0 : 1;
+}
+]];
+
+local function c_escape(str)
+	local parts = {};
+	for i = 1, math.ceil(#str / 32) do
+		table.insert(parts, "\"" .. str:sub((i - 1) * 32 + 1, i * 32):gsub(".", function (c)
+			local b = string.byte(c);
+
+			if c == "\n" then
+				return "\\n";
+			elseif c == "\\" then
+				return "\\\\";
+			elseif c == "\"" then
+				return "\\\"";
+			elseif b >= 32 and b <= 126 then
+				return c;
+			else
+				return ("\\%.3o"):format(b);
+			end
+		end) .. "\"");
+	end
+
+	return table.concat(parts, "\n\t\t");
+end
+
+--- @param src string
+local function find_lua(src)
+	return src:gmatch "require%s*%(?%s*[\"']([^\n\\\"']-)[\"']%s*%)?";
+end
+--- @param src string
+local function find_ffi(src)
+	return src:gmatch "ffi%.load%s*%(?%s*[\"']([^\n\\\"']-)[\"']%s*%)?";
+end
+
+--- @param ctx tal.mklua.ctx
+--- @param name string
+local function resolve_lua(ctx, name)
+	local path;
+	path = package.searchpath(name, ctx.path or package.path);
+	if path then
+		return "lua", path;
+	end
+
+	path = package.searchpath(name, ctx.cpath or package.cpath);
+	if path then
+		return "c", path;
+	end
+
+	return nil;
+end
+--- @param ctx tal.mklua.ctx
+--- @param dep string
+local function resolve_ffi(ctx, dep)
+	local path = package.searchpath(dep, ctx.ffi_path or ffi.path);
+	if not path then
+		return nil;
+	end
+
+	if path:find "%.dll$" or path:find "%.so$" then
+		return path, (path:gsub("%.[dlso]-$", ".a"));
+	end
+
+	return path;
+end
+
+--- @param ctx tal.mklua.ctx
+--- @param name string
+--- @param filename string
+--- @param func? function
+--- @param out tal.mklua.out
+--- @param passed table<string, string>
+local function emit_lua(ctx, name, filename, func, out, passed)
+	if passed[name] then return end
+
+	local dep_libs = {};
+
+	if not func then
+		local f = assert(io.open(filename, "r"));
+		local src = assert(f:read "a");
+		f:close();
+
+		for dep in find_lua(src) do
+			local kind, path = resolve_lua(ctx, dep);
+
+			if out.deps then
+				out.deps[dep] = path;
+			end
+
+			if kind == "lua" then
+				emit_lua(ctx, dep, path --[[@as string]], func, out, passed);
+			elseif kind == "c" then
+				table.insert(dep_libs, dep);
+				passed[name] = "luaopen_" .. name:gsub("[%.%-]", "_");
+			elseif not kind then
+				io.stderr:write("Module '" .. dep .. "' could not be resolved!\n");
+			end
+		end
+
+		for dep in find_ffi(src) do
+			local path, path_a = resolve_ffi(ctx, dep);
+
+			if out.ffi_deps then
+				out.ffi_deps[dep] = path;
+			end
+
+			if not path then
+				io.stderr:write("FFI library '" .. dep .. "' could not be resolved!\n");
+			elseif path_a and out.alibs then
+				table.insert(out.alibs, path_a);
+			elseif path and out.libs then
+				table.insert(out.libs, path_a);
+			end
+		end
+
+		func = assert(load(src, "@" .. filename, "t"));
+	end
+
+	local funcname = "talb_open_" .. name:gsub("[%.%-]", "_");
+
+	local bc = string.dump(func, not ctx.g);
+
+	if out.f then
+		out.f:write("TALB_INTERNAL int " .. funcname .. "(lua_State *ctx) {\n");
+
+		for i = 1, #dep_libs do
+			out.f:write(("\ttalb_register(ctx, %q, %s);\n"):format(dep_libs[i], "luaopen_" .. dep_libs[i]:gsub("%.", "_")));
+		end
+
+		out.f:write (("\n\tif (luaL_loadbufferx(ctx, %s, %d, %q, \"b\")) {\n"):format(c_escape(bc), #bc, "@" .. filename));
+		out.f:write ("\t\tluaL_error(ctx, \"lua bytecode error: %s\", lua_tostring(ctx, -1));\n");
+		out.f:write ("\t}\n\n");
+		out.f:write (("\tlua_pushstring(ctx, %q);\n"):format(name));
+		out.f:write (("\tlua_pushstring(ctx, %q);\n"):format(filename));
+		out.f:write ("\tlua_call(ctx, 2, 1);\n");
+		out.f:write ("\treturn 1;\n");
+		out.f:write ("}\n");
+	end
+
+	passed[name] = funcname;
+end
+
+--- @param ctx tal.mklua.ctx
+--- @param name string
+--- @param out tal.mklua.out
+--- @param passed table<string, string>
+local function emit_luaopen(ctx, name, out, passed)
+	local kind, path = resolve_lua(ctx, name);
+	if kind == "lua" then
+		emit_lua(ctx, name, path --[[@as string]], nil, out, passed);
+		passed[name] = "talb_open_" .. name:gsub("[%.%-]", "_");
+	elseif kind == "c" then
+		passed[name] = "luaopen_" .. name:gsub("[%.%-]", "_");
+	else
+		error("entry '" .. name .. "' couldn't be resolved");
+	end
+
+	if out.deps then
+		out.deps[name] = path;
+	end
+
+	if out.f then
+		out.f:write("TALB_EXPORT int luaopen_", name:gsub("[.%-]", "_"), "(lua_State *ctx) {\n");
+		out.f:write("\treturn ", passed[name], "(ctx);\n");
+		out.f:write("}\n\n");
+	end
+end
+
+--- @param ctx tal.mklua.ctx
+--- @param out tal.mklua.out
+local function gen(ctx, out)
+	if out.f then
+		out.f:write [[/* Generated by tal bundle */
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+
+#ifndef TALB_INTERNAL
+	#define TALB_INTERNAL static
+#endif
+#ifndef TALB_EXPORT
+	#ifdef TALB_HEADER
+		#define TALB_EXPORT static
+	#else
+		#define TALB_EXPORT extern
+	#endif
+#endif
+
+TALB_INTERNAL void talb_register(lua_State *ctx, const char *modname, lua_CFunction openf) {
+	lua_getfield(ctx, LUA_REGISTRYINDEX, "_PRELOAD");
+	lua_pushcfunction(ctx, openf);
+	lua_setfield(ctx, -2, modname);
+	lua_pop(ctx, 1);
+}
+
+]];
+	end
+
+	--- @type table<string, string>
+	local passed = {};
+
+	for i = 1, #ctx.entries do
+		emit_luaopen(ctx, ctx.entries[i], out, passed);
+	end
+
+	if ctx.entry and out.f then
+		local register_calls = {};
+
+		for i = 2, #ctx.entries do
+			local name = ctx.entries[i];
+			table.insert(register_calls, ("talb_register(ctx, %q, %s);"):format(name, passed[name]))
+		end
+
+		emit_lua(ctx, "__err_handle", "<internal>", function () return debug.traceback end, { f = out.f }, passed);
+
+		local args_str = {};
+		local args = ctx.args or {};
+
+		for i = 1, #args do
+			table.insert(args_str, ("\tlua_pushstring(ctx, %q);"):format(args[i]));
+		end
+
+		out.f:write(entry_format:format(
+			#args,
+			table.concat(register_calls, "\n\t\t"),
+			passed["__err_handle"], passed[ctx.entries[1]],
+			table.concat(args_str, "\n"),
+			#args
+		));
+
+		if ctx.main then
+			out.f:write(main_format);
+		end
+	end
+
+	return passed;
+end
+
+local function open_w(path)
+	if type(path) == "string" then
+		if path == "-" then
+			return io.stdout, function () end
+		else
+			local f = assert(io.open(path, "w"));
+			return f, f.close;
+		end
+	else
+		return assert(path);
+	end
+end
+
+return {
+	main = args.cli {
+		ctx = {
+			entries = {},
+		},
+		flags = {
+			help = args.bool "help",
+			debug = args.bool "debug",
+			main = args.bool "main",
+			entry = args.bool "entry",
+			header = args.bool "header",
+
+			deps = args.bool "deps",
+			libs = args.bool "libs",
+
+			path = args.str "path",
+			cpath = args.str "cpath",
+			["ffi-path"] = args.str "ffi_path",
+			args = args.rest "args",
+
+			output = args.str "output",
+			makefile = args.str "makefile",
+
+			o = "output",
+			g = "debug",
+			h = "help",
+		},
+		rest = args.str_arr "entries",
+		next = function (ctx)
+			if ctx.help or not ctx.cpath and not ctx.libs and not ctx.deps and not ctx.output then
+				print(help_msg);
+				return;
+			end
+
+			if ctx.deps then
+				local deps = {};
+				gen(ctx, { deps = deps });
+				for k, v in pairs(deps) do
+					io.stdout:write(v .. "\n");
+				end
+			elseif ctx.libs then
+				local libs = {};
+				gen(ctx, { libs = libs });
+				for i = 1, #libs do
+					io.stdout:write(libs[i] .. "\n");
+				end
+			elseif ctx.output then
+				local f, close = open_w(ctx.output);
+				gen(ctx, { f = f });
+				close(f);
+			end
+		end
+	},
+	gen = gen,
+	open_w = open_w,
+};
+
