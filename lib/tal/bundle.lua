a0b0f8d44ab8f2b05494999ccd780775cf51e83a
TopchetoEU <36534413+TopchetoEU@users.noreply.github.com>
2026-03-06 05:01:05 +0200

    add compile command presets to tal.bundle

--- lib/tal/bundle.lua
+++ lib/tal/bundle.lua
@@ -1,6 +1,7 @@
 local args = require "std.fmt.args";
 local mklua= require "tal.mklua"
 local spawn = require "std.proc";
+local ffi   = require "ffi"
 
 local help_msg = [[tal bundle by TopchetoEU
 
@@ -10,9 +11,14 @@ Flags:
 
 <entry> - the name of a lua module to be bundled
 
---compile-cmd (-c) ... - specifies the rest of the arguments as a compiler, to be used to produce the executable.
+--compile-cmd (-C) ... - specifies the rest of the arguments as a compiler, to be used to produce the executable.
 	The static libraries, used by the lua module will be appended to the arguments.
 	The contents of the generated C file will be piped in stdin
+--compiler (-c) - sets compile-cmd to a preexisting compile command preset:
+	- gcc - A gcc-like command (uses cc for clang compat)
+	- msvc - An msvc-like command. Not tested, not recommended
+	If --output is specified in conjunction with this, it will translate to an output flag for the compile command.
+
 --output (-o) <file> - specifies the file to which the C source should be written
 --bootstrap <module> - specifies an alternative to "tal.entry" that will bootstrap the compiled module
 ]]
@@ -22,23 +28,47 @@ return args.cli {
 	flags = {
 		help = args.bool "help",
 		["compile-cmd"] = args.rest "compile_cmd",
+		compiler = args.str "compiler",
 		output = args.str "output",
+		debug = args.bool "debug",
 		deps = args.bool "deps",
 		libs = args.bool "libs",
 
 		bootstrap = args.str "bootstrap",
 
-		c = "compile-cmd",
+		C = "compile-cmd",
+		c = "compiler",
 		o = "output",
+		g = "debug",
 	},
 	rest = args.str "entry",
 	next = function (ctx)
-		if ctx.help then
+		if ctx.help or not ctx.compile_cmd and not ctx.compiler and not ctx.output then
 			return io.stderr:write(help_msg);
 		end
 
 		if not ctx.entry then
 			return io.stderr:write "error: an entry must be specified\n";
+		end
+
+		if ctx.compiler then
+			if ctx.compiler == "gcc" then
+				ctx.compile_cmd = { "cc", "-x", "c", "-", "-x", "none", "-lm" };
+
+				if ctx.output then
+					table.insert(ctx.compile_cmd, "-o");
+					table.insert(ctx.compile_cmd, ctx.output);
+				end
+			elseif ctx.compiler == "msvc" then
+				ctx.compile_cmd = { "cl", "/Tc", "-" };
+
+				if ctx.output then
+					table.insert(ctx.compile_cmd, "/Fe:" .. ctx.output);
+				end
+			else
+				io.stderr:write "invalid compiler preset\n";
+				return;
+			end
 		end
 
 		local mklua_ctx = {
@@ -49,18 +79,46 @@ return args.cli {
 			entry = true,
 			main = true,
 			noinc = true,
+			debug = ctx.debug,
 		};
 
 		if ctx.compile_cmd then
+			local lj_lib, err_a, err_so;
+			lj_lib, err_a = package.searchpath("luajit", ffi.apath);
+			if not lj_lib then
+				lj_lib, err_so = package.searchpath("luajit", ffi.path)
+				if not lj_lib then
+					io.stderr:write("luajit not found in ffi path:\n" .. err_a .. "\n" .. err_so);
+					return;
+				end
+			end
+
+			table.insert(ctx.compile_cmd, lj_lib)
+
 			local comp_proc = assert(spawn {
 				argv = ctx.compile_cmd,
 				stdin = "pipe",
 				env = { PATH = os.getenv "PATH" or "" }
 			});
 
-			mklua.gen(mklua_ctx, { f = assert(comp_proc.stdin) --[[@as file*]] });
+			local libs = {};
+
+			mklua.gen(mklua_ctx, { f = comp_proc.stdin --[[@as file*]], libs = libs });
+
 			comp_proc.stdin:close();
 			assert(comp_proc:wait());
+
+			if #libs > 0 then
+				io.stderr:write [[
+The lua code depends on some native libraries. For luajit's ffi to find them,
+you must either 1. include them in the cwd (usually next to the executable),
+2. install the libraries on the targeted system or 3. install the libraries to
+a known path and then set the env variable FFI_PATH, so that ffi can find the
+libraries.
+
+For easier distribution, bundle the executable + the libraries in an appimage.
+]];
+			end
 		elseif ctx.output then
 			local f, close = mklua.open_w(ctx.output);
 			mklua.gen(mklua_ctx, { f = f });
@@ -70,20 +128,16 @@ return args.cli {
 
 			mklua.gen(mklua_ctx, { libs = libs });
 
-			if ctx.libs then
-				for i = 1, #libs do
-					io.stdout:write(libs[i] .. "\n");
-				end
+			for i = 1, #libs do
+				io.stdout:write(libs[i] .. "\n");
 			end
 		elseif ctx.deps then
 			local deps = {};
 
 			mklua.gen(mklua_ctx, { deps = deps });
 
-			if ctx.deps then
-				for k, v in pairs(deps) do
-					io.stdout:write(v .. "\n");
-				end
+			for k, v in pairs(deps) do
+				io.stdout:write(v .. "\n");
 			end
 		else
 			io.stderr:write "--compile-cmd or --output must be specified\n";
