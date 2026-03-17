eac1f100bfa9afcc8e4a39df23bbdcf816bddd54
TopchetoEU <36534413+TopchetoEU@users.noreply.github.com>
2026-03-18 01:12:25 +0200

    improve how stream backends are handles

--- lib/std/io/stream.lua
+++ lib/std/io/stream.lua
@@ -1,29 +1,28 @@
 local lines = require "std.io.lines";
 local collected = require "std.collected";
 
---- @class std.io.stream_funcs
---- @field read fun(self, n: integer): string?, string?
+--- @class std.io.stream_backend
+--- @field read fun(self, n?: integer): string?, string?
 --- @field write fun(self, data: string): true?, string?
 --- @field seek? fun(self, pos: integer, whence: "set" | "cur" | "end"): integer?, string?
 --- @field sync? fun(self): true?, string?
 --- @field close fun(self)
 
 --- @class std.io.stream
---- @field _fd any
---- @field _funcs std.io.stream_funcs
+--- @field _backend std.io.stream_backend
 --- @field _buff string[]
 --- @field _mngd boolean | string? If managed (aka not closed by the owner), this is set to true or a stack trace
 local stream_index = {};
 
---- @param fmt "a" | "l" | "L" | integer
+--- @param fmt? "a" | "c" | "l" | "L" | integer
 function stream_index:read(fmt)
-	if self._funcs.seek then
-		return lines.seekable(self._fd, self._funcs.read, self._funcs.seek, fmt);
+	if self._backend.seek then
+		return lines.seekable(self._backend, fmt);
 	else
-		return lines.chunked(self._fd, self._funcs.read, self._buff, fmt);
+		return lines.chunked(self._backend, self._buff, fmt);
 	end
 end
---- @param fmt "a" | "l" | "L" | integer
+--- @param fmt? "a" | "c"  | "l" | "L" | integer
 function stream_index:lines(fmt)
 	return function ()
 		local res, err = self:read(fmt);
@@ -34,25 +33,25 @@ function stream_index:lines(fmt)
 end
 --- @param ... string | integer
 function stream_index:write(...)
-	return self._funcs.write(self._fd, table.concat { ... });
+	return self._backend:write(table.concat { ... });
 end
 --- @param pos integer
 --- @param whence "set" | "cur" | "end"
 function stream_index:seek(pos, whence)
-	if not self._funcs.seek then
+	if not self._backend.seek then
 		return nil, "seeking not supported";
 	end
 
-	return self._funcs.seek(self._fd, pos, whence);
+	return self._backend:seek(pos, whence);
 end
 function stream_index:sync()
-	if not self._funcs.sync then return true end
-	return self._funcs.sync(self._fd);
+	if not self._backend.sync then return true end
+	return self._backend:sync();
 end
 stream_index.flush = stream_index.sync;
 function stream_index:close()
 	self._mngd = nil;
-	return self._funcs.close(self._fd);
+	return self._backend:close();
 end
 
 local stream_meta = {
@@ -71,14 +70,12 @@ function stream_meta:__gc()
 	end
 end
 
---- @param hnd any
---- @param funcs? std.io.stream_funcs
+--- @param backend? std.io.stream_backend
 --- @param mngd? string | true
 --- @return std.io.stream
-local function new(hnd, funcs, mngd)
+local function new(backend, mngd)
 	return collected(setmetatable({
-		_fd = hnd,
-		_funcs = funcs or hnd;
+		_backend = backend,
 		_buff = {},
 		_mngd = mngd,
 	}, stream_meta));
@@ -87,25 +84,38 @@ end
 --- @param read std.io.stream
 --- @param write std.io.stream
 local function combine(read, write)
-	local funcs = {};
-	function funcs:read(n)
-		return self[1]:read(n);
+	local funcs = {
+		read_str = read,
+		write_str = write,
+	};
+	function funcs:read()
+		if not self.read_str then return nil, "closed" end
+		return self.read_str:read();
 	end
 	function funcs:write(data)
-		return self[2]:write(data);
+		if not self.write_str then return nil, "closed" end
+		return self.write_str:write(data);
 	end
 	function funcs:sync()
-		local ok, err = self[1]:sync();
+		if not self.read_str then return nil, "closed" end
+		local ok, err = self.read_str:sync();
 		if not ok then return nil, err end
 
-		local ok, err = self[2]:sync();
+		if not self.write_str then return nil, "closed" end
+		local ok, err = self.write_str:sync();
 		if not ok then return nil, err end
 
 		return true;
 	end
 	function funcs:close()
-		self[1]:close();
-		self[2]:close();
+		if self.read_str then
+			self.read_str:close();
+			self.read_str = nil;
+		end
+		if self.write_str then
+			self.write_str:close();
+			self.write_str = nil;
+		end
 	end
 
 	local mngd = read._mngd or write._mngd;
@@ -113,7 +123,7 @@ local function combine(read, write)
 		mngd = write._mngd;
 	end
 
-	return new({ read, write }, funcs, read._mngd and write._mngd);
+	return new(funcs, mngd);
 end
 
 return { new = new, combine = combine };
