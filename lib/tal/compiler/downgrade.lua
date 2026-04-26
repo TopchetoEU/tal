2775cc5bac1a98d691fad4e340fa85d363c2075a
TopchetoEU <36534413+TopchetoEU@users.noreply.github.com>
2026-04-27 01:28:33 +0300

    improve compiler

---
+++ lib/tal/compiler/downgrade.lua
@@ -0,0 +1,254 @@
+local nodes = require "tal.compiler.node";
+local syntax = require "tal.compiler.syntax";
+local walk   = require "tal.compiler.walk"
+
+--- @class compiler.downgrade.ctx
+--- @field parts node.stm[]
+--- @field id_base string
+--- @field next_id integer
+--- @field polyfills table<string, node.name> A table polyfill name -> polyfill
+
+--- @class compiler.downgrade.scope
+--- @field prev compiler.downgrade.scope?
+--- @field names table<string, node.name>
+--- @field consts table<string, boolean>
+
+local interop_funcs = {
+	[nodes.ops.POW] = syntax.parse_exp("math.pow", true),
+
+	[nodes.ops.B_NEG] = syntax.parse_exp("bit.bnot", true),
+	[nodes.ops.IDIV] = syntax.parse_exp([[function (a, b)
+		return math.floor(a / b);
+	end]], true), -- TODO: to be done
+
+	[nodes.ops.B_SHL] = syntax.parse_exp("bit.lshift", true),
+	[nodes.ops.B_SHR] = syntax.parse_exp("bit.rshift", true),
+	[nodes.ops.B_AND] = syntax.parse_exp("bit.band", true),
+	[nodes.ops.B_OR] = syntax.parse_exp("bit.bor", true),
+	[nodes.ops.B_XOR] = syntax.parse_exp("bit.bxor", true),
+};
+local polyfills = {
+	getfenv = syntax.parse_exp("getfenv", true),
+	setfenv = syntax.parse_exp("setfenv", true),
+};
+
+--- @param self compiler.downgrade.ctx
+--- @param name string
+--- @param node node.exp
+local function polyfill(self, name, node)
+	if self.polyfills[name] then
+		return self.polyfills[name];
+	end
+
+	local id = self.id_base .. "_" .. self.next_id;
+	self.next_id = self.next_id + 1;
+	local res_name = node.name(nil, id, false);
+
+	table.insert(self.parts, nodes.decl(nil, false, { res_name }, { node }));
+	self.polyfills[name] = res_name;
+
+	return res_name;
+end
+
+--- @param self compiler.downgrade.scope
+--- @param names node.name[]
+local function walk_var_decls(self, names, is_const)
+	for i = 1, #names do
+		self.names[names[i].name] = names[i];
+		self.consts[names[i].name] = is_const or false;
+	end
+end
+
+--- @param self compiler.downgrade.scope
+local function scope_child(self)
+	return {
+		consts = {},
+		names = {},
+		prev = self,
+	} --[[@as compiler.downgrade.scope]];
+end
+
+--- @param self compiler.downgrade.scope
+--- @param name string
+local function scope_find(self, name)
+	local curr = self;
+
+	while curr do
+		if curr.names[name] then
+			return curr.names[name], curr.consts[name];
+		end
+
+		curr = curr.prev;
+	end
+
+	return nil;
+end
+
+--- @param self compiler.downgrade.scope
+local function scope_tmp(self)
+	local i = 0;
+	repeat
+		i = i + 1;
+	until not scope_find(self, "_" .. i);
+
+	--- @cast i integer
+
+	return nodes.name(nil, "_" .. i, false);
+end
+
+local walker = walk(
+	--- @param ctx compiler.downgrade.ctx
+	--- @param scope compiler.downgrade.scope
+	function (self, node, target, ctx, scope)
+		if node.type == "var" then
+			if node.name == "_ENV" then
+				return nodes.call(node.loc,
+					nodes.var(node.loc, polyfill(ctx, "getfenv", polyfills.getfenv)),
+					nodes.int(node.loc, 1)
+				);
+			end
+
+			local env_var = scope_find(scope, "_ENV");
+			if env_var then
+				return nodes.index(node.loc, nodes.var(node.loc, env_var), nodes.str(node.loc, node.name.name));
+			end
+		elseif node.type == "op" then
+			local func = interop_funcs[node.op];
+			if func then
+				node.a = self:walk_exp(node.a, "multi", ctx, scope);
+				node.b = node.b and self:walk_exp(node.b, "multi", ctx, scope);
+
+				local id = polyfill(ctx, "operator_" .. node.op, func);
+				return nodes.call(node.loc, nodes.var(node.loc, id), node.a, node.b);
+			end
+		end
+
+		if node.type == "func" then
+			local child = scope_child(scope);
+			walk_var_decls(child, node.args, false);
+			node.body = self:walk_body(node.body, ctx, child);
+			return node;
+		end
+	end,
+	--- @param ctx compiler.downgrade.ctx
+	--- @param scope compiler.downgrade.scope
+	function (self, node, ctx, scope)
+		if node.type == "assign" then
+			--- @type integer[]
+			local globs = {};
+
+			for i = 1, #node.targets do
+				local t = node.targets[i];
+				if t.type == "name" and t.name == "_ENV" and t.global then
+					table.insert(globs, t);
+				end
+			end
+
+			local env_var = scope_find(scope, "_ENV");
+			if #globs > 0 and env_var then
+				local tmp = scope_tmp(scope);
+
+				for i = 1, #globs do
+					node.targets[globs[i]] = tmp;
+				end
+
+				node.targets = self:walk_multiexp(node.targets, #node.targets, ctx, scope);
+				node.values = self:walk_multiexp(node.values, #node.targets, ctx, scope);
+
+				return {
+					nodes.decl(node.loc, false, { tmp });
+					node,
+					nodes.call(node.loc,
+						nodes.var(node.loc, polyfill(ctx, "setfenv", polyfills.setfenv)),
+						nodes.int(node.loc, 0),
+						nodes.var(node.loc, tmp)
+					),
+				};
+			end
+		end
+
+		if node.type == "decl" then
+			if node.pre then
+				walk_var_decls(scope, node.names, false);
+			end
+			if node.values then
+				node.values = self:walk_multiexp(node.values, #node.names, ctx, scope);
+			end
+			if node.pre then
+				walk_var_decls(scope, node.names, false);
+			end
+
+			return node;
+		elseif node.type == "if" then
+			for i = 1, #node.bodies do
+				node.conds[i] = self:walk_exp(node.conds[i], "bool", ctx, scope);
+				node.bodies[i] = self:walk_body(node.bodies[i], ctx, scope_child(scope));
+			end
+
+			if node.default then
+				node.default = self:walk_body(node.default, ctx, scope);
+			end
+
+			return node;
+		elseif node.type == "while" then
+			node.cond = self:walk_exp(node.cond, "bool", ctx, scope);
+			node.body = self:walk_body(node.body, ctx, scope_child(scope));
+			return node;
+		elseif node.type == "repeat" then
+			local child = scope_child(scope);
+			node.body = self:walk_body(node.body, ctx, child);
+			node.cond = self:walk_exp(node.cond, "bool", ctx, child);
+			return node;
+		elseif node.type == "for" then
+			node.first = self:walk_exp(node.first, "multi", ctx, scope);
+			if node.last then
+				node.last = self:walk_exp(node.last, "multi", ctx, scope);
+			end
+			if node.step then
+				node.step = self:walk_exp(node.step, "multi", ctx, scope);
+			end
+			local child = scope_child(scope);
+			walk_var_decls(child, { node.name }, false);
+			node.body = self:walk_body(node.body, ctx, child);
+			return node;
+		elseif node.type == "for_in" then
+			local child = scope_child(scope);
+			walk_var_decls(child, node.names, false);
+			node.values = self:walk_multiexp(node.values, #node.names);
+			node.body = self:walk_body(node.body, ctx, child);
+			return node;
+		elseif node.type == "scope" then
+			node.body = self:walk_body(node.body, ctx, scope_child(scope));
+			return node;
+		end
+	end
+);
+
+
+return {
+	--- @param body node.stm[]
+	walk_body = function (body)
+		return walker:walk_body(body,
+			{
+				id_base = "_" .. math.random(1, 0x1000000),
+				next_id = 1,
+				parts = {},
+				polyfills = {},
+			} --[[@as compiler.downgrade.ctx]],
+			{ prev = nil, consts = {}, names = {} } --[[@as compiler.downgrade.scope]]
+		);
+	end,
+	--- @param exp node.exp
+	--- @param target compiler.walk.target
+	walk_exp = function (exp, target)
+		return walker:walk_exp(exp, target,
+			{
+				id_base = "_" .. math.random(1, 0x1000000),
+				next_id = 1,
+				parts = {},
+				polyfills = {},
+			} --[[@as compiler.downgrade.ctx]],
+			{ prev = nil, consts = {}, names = {} } --[[@as compiler.downgrade.scope]]
+		);
+	end
+};
---
+++ lib/tal/compiler/walk.lua
@@ -0,0 +1,246 @@
+local ops = require "tal.compiler.node".ops;
+
+--- @alias compiler.walk.target "multi" | "single" | "bool"
+
+--- @class compiler.walk.ctx
+--- @field walk_exp fun(self, node: node.exp, target: compiler.walk.target, ...): node.exp
+--- @field walk_multiexp fun(self, nodes: node.exp[], target?: integer, ...): node.exp[]
+--- @field walk_body fun(self, node: node.stm[], ...): node.stm[]
+
+--- @param exp_cb? fun(self: compiler.walk.ctx, node: node.exp, target: compiler.walk.target, ...): node.exp | false?
+--- @param stm_cb? fun(self: compiler.walk.ctx, node: node.stm, ...): node.stm | node.stm[] | false?
+return function (exp_cb, stm_cb)
+	--- @type table<string, fun(self, node: node.exp, target: compiler.walk.target, ...): node.exp>
+	local exp_walkers = {};
+	--- @type table<string, fun(self, node: node.stm, ...): node.stm>
+	local stm_walkers = {};
+
+	--- @param res node.stm[]
+	--- @param curr node.stm | node.stm[]
+	local function push_to_res(res, curr)
+		if curr.type then
+			table.insert(res, curr);
+		else
+			table.move(curr, 1, #curr, #res + 1, res);
+		end
+
+		return res;
+	end
+
+	--- @type compiler.walk.ctx
+	local self = {
+		walk_exp = function (self, node, target, ...)
+			if exp_cb then
+				local res = exp_cb(self, node, target, ...)
+				if res then return res end
+			end
+
+			return exp_walkers[node.type](self, node, target, ...);
+		end,
+		walk_body = function (self, body, ...)
+			local res = {};
+
+			for i = 1, #body do
+				local raw = body[i];
+				local curr_res;
+
+				if stm_walkers[raw.type] then
+					if stm_cb then
+						curr_res = stm_cb(self, raw, ...);
+					end
+
+					if not curr_res then
+						curr_res = stm_walkers[raw.type](self, raw, ...);
+					end
+				else
+					if exp_cb then
+						curr_res = exp_cb(self, raw --[[@as node.exp]], "multi", ...) --[[@as node.stm]];
+					end
+
+					if not curr_res then
+						curr_res = exp_walkers[raw.type](self, raw --[[@as node.exp]], "multi", ...) --[[@as node.stm]];
+					end
+				end
+
+				push_to_res(res, curr_res);
+			end
+
+			return res;
+		end,
+		walk_multiexp = function (self, nodes, target, ...)
+			-- We can safely omit the parens on the last expression, if we overflow or match the required expressions
+			local multi_safe = target and #nodes >= target;
+
+			for i = 1, #nodes do
+				if i == #nodes and not multi_safe then
+					nodes[i] = self:walk_exp(nodes[i], "single", ...);
+				else
+					nodes[i] = self:walk_exp(nodes[i], "multi", ...);
+				end
+			end
+
+			return nodes;
+		end,
+	};
+
+	-- function exp_walkers.name(self, node, target)
+	-- 	return node;
+	-- end
+	function exp_walkers.var(self, node, target)
+		return node;
+	end
+	function exp_walkers.str(self, node, target)
+		return node;
+	end
+	exp_walkers["nil"] = function (self, node, target)
+		return node;
+	end
+	function exp_walkers.bool(self, node, target)
+		return node;
+	end
+	function exp_walkers.int(self, node, target)
+		return node;
+	end
+	function exp_walkers.fl(self, node, target)
+		return node;
+	end
+	function exp_walkers.args(self, node, target)
+		return node;
+	end
+	--- @param node node.paren
+	function exp_walkers.paren(self, node, target, ...)
+		node.val = self:walk_exp(node.val, "multi", ...);
+		return node;
+	end
+	--- @param self compiler.walk.ctx
+	--- @param node node.table
+	function exp_walkers.table(self, node, target, ...)
+		self:walk_multiexp(node.keys, #node.keys, ...);
+		self:walk_multiexp(node.vals, #node.vals, ...);
+		self:walk_multiexp(node.arr, nil, ...);
+		return node;
+	end
+	--- @param node node.func
+	function exp_walkers.func(self, node, target, ...)
+		node.body = self:walk_body(node.body, ...);
+		return node;
+	end
+	--- @param node node.op
+	function exp_walkers.op(self, node, target, ...)
+		if
+			node.op == ops.NOT or
+			node.op == ops.AND or
+			node.op == ops.OR
+		then
+			node.a = self:walk_exp(node.a, "bool", ...);
+			if node.b then
+				node.b = self:walk_exp(node.b, "bool", ...);
+			end
+
+			return node;
+		else
+			node.a = self:walk_exp(node.a, "multi", ...);
+			if node.b then
+				node.b = self:walk_exp(node.b, "multi", ...);
+			end
+
+			return node;
+		end
+	end
+	--- @param self compiler.walk.ctx
+	--- @param node node.call
+	function exp_walkers.call(self, node, target, ...)
+		self:walk_multiexp(node, nil, ...);
+		node.func = self:walk_exp(node.func, "multi", ...);
+		return node;
+	end
+	--- @param self compiler.walk.ctx
+	--- @param node node.method
+	function exp_walkers.method(self, node, target, ...)
+		self:walk_multiexp(node, nil, ...);
+		node.obj = self:walk_exp(node.obj, "multi", ...);
+		return node;
+	end
+	--- @param node node.index
+	function exp_walkers.index(self, node, target, ...)
+		node.obj = self:walk_exp(node.obj, "multi", ...);
+		node.key = self:walk_exp(node.key, "multi", ...);
+		return node;
+	end
+
+	--- @param node node.decl
+	function stm_walkers.decl(self, node, ...)
+		if node.values then
+			self:walk_multiexp(node.values, #node.names, ...);
+		end
+
+		return node;
+	end
+	--- @param node node.assign
+	function stm_walkers.assign(self, node, ...)
+		self:walk_multiexp(node.targets, #node.targets, ...);
+		self:walk_multiexp(node.values, #node.targets, ...);
+
+		return node;
+	end
+	--- @param node node.if
+	stm_walkers["if"] = function (self, node, ...)
+		for i = 1, #node.bodies do
+			node.conds[i] = self:walk_exp(node.conds[i], "bool", ...);
+			node.bodies[i] = self:walk_body(node.bodies[i], ...);
+		end
+
+		if node.default then
+			node.default = self:walk_body(node.default, ...);
+		end
+
+		return node;
+	end
+	--- @param node node.while
+	stm_walkers["while"] = function (self, node, ...)
+		node.cond = self:walk_exp(node.cond, "bool", ...);
+		node.body = self:walk_body(node.body, ...);
+		return node;
+	end
+	--- @param node node.while
+	stm_walkers["repeat"] = function (self, node, ...)
+		node.body = self:walk_body(node.body, ...);
+		node.cond = self:walk_exp(node.cond, "bool", ...);
+		return node;
+	end
+	--- @param node node.for
+	stm_walkers["for"] = function (self, node, ...)
+		node.first = self:walk_exp(node.first, "single", ...);
+		if node.last then
+			node.last = self:walk_exp(node.last, "single", ...);
+		end
+		if node.step then
+			node.step = self:walk_exp(node.step, "single", ...);
+		end
+		node.body = self:walk_body(node.body, ...);
+		return node;
+	end
+	--- @param node node.for_in
+	function stm_walkers.for_in(self, node, ...)
+		self:walk_multiexp(node.values, #node.names, ...);
+		node.body = self:walk_body(node.body, ...);
+		return node;
+	end
+	--- @param node node.scope
+	function stm_walkers.scope(self, node, ...)
+		node.body = self:walk_body(node.body, ...);
+		return node;
+	end
+
+	--- @param node node.return
+	stm_walkers["return"] = function (self, node, ...)
+		self:walk_multiexp(node, nil, ...);
+		return node;
+	end
+	--- @param node node.break
+	stm_walkers["break"] = function (self, node) return node end
+	--- @param node node.break
+	stm_walkers["continue"] = function (self, node) return node end
+
+	return self;
+end
