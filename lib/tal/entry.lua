96bcb2c3d138bfc0d5fbb2d97dd724a9ebd32db3
TopchetoEU <36534413+TopchetoEU@users.noreply.github.com>
2026-04-27 01:28:52 +0300

    improve entry

--- lib/tal/entry.lua
+++ lib/tal/entry.lua
@@ -1,29 +1,30 @@
-require "tal.globs.errors";
+require "std.error";
 require "nat.ffi";
-local ev = require "nat.libev";
-local loop = require "tal.loop";
 
 local has_dbg, dbg = pcall(require, "lldebugger");
 if has_dbg then dbg.start() end
 
 return function (...)
-	return assert(loop.run(ev.new(), function (...)
-		local env = setmetatable({}, { __index = _G });
-		env._G = env;
-		env._ENV = env;
-		setfenv(0, env);
+	local env = setmetatable({}, { __index = _G });
+	env._G = env;
+	env._ENV = env;
+	setfenv(0, env);
 
-		require "tal.globs";
-		local entry = require "tal.cli";
+	require "tal.globs";
+	local loop = require "std.loop";
+	local entry = require "tal.cli";
 
-		if type(entry) == "table" then
-			if type(entry.__main) == "function" then
-				return entry.main(...);
-			elseif type(entry.main) == "function" then
-				return entry.main(...);
-			end
+	loop.name(coroutine.running(), "Main thread");
+
+	if type(entry) == "table" then
+		if type(entry.__main) == "function" then
+			entry.main(...);
+		elseif type(entry.main) == "function" then
+			entry.main(...);
 		end
+	else
+		entry(...);
+	end
 
-		return entry(...);
-	end, ...));
+	return assert(loop.run());
 end
