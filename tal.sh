6cea1359b8755f8a9f52239cdc395c37c9b3241c
TopchetoEU <36534413+TopchetoEU@users.noreply.github.com>
2026-05-16 00:27:57 +0300

    use correct paths in tal.sh

--- tal.sh
+++ tal.sh
@@ -2,6 +2,15 @@
 
 ROOT=$(dirname "$0")
 
-export FFI_PATH="$ROOT/deps/luajit/src/lib?.so;$ROOT/deps/libev/bin/Linux/lib?.so;;"
-export LUA_PATH="$ROOT/lib/?.lua;$ROOT/lib/?/init.lua;$LUA_PATH"
+for el in "?" "?.so" "lib?.so"; do
+	FFI_PATH="$ROOT/deps/luajit/src/$el;$ROOT/deps/libev/bin/Linux/$el;$FFI_PATH"
+done
+for el in "?" "?.a" "lib?.a"; do
+	FFI_APATH="$ROOT/deps/luajit/src/$el;$ROOT/deps/libev/bin/Linux/$el;$FFI_APATH"
+done
+
+export FFI_PATH="$FFI_PATH;;"
+export FFI_APATH="$FFI_APATH;;"
+export LUA_PATH="$ROOT/deps/luajit/src/?.lua;$ROOT/deps/luajit/src/?/init.lua;$LUA_PATH;;"
+export LUA_PATH="$ROOT/lib/?.lua;$ROOT/lib/?/init.lua;$LUA_PATH;;"
 exec "$ROOT/deps/luajit/src/luajit" "$ROOT/lib/tal.lua" "$@"
