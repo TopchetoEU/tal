1ce95d628c473c7d52e0a3c7edfc2cbc42ad3e89
TopchetoEU <36534413+TopchetoEU@users.noreply.github.com>
2026-05-24 13:52:47 +0300

    bump libev; improve tal.sh

--- tal.sh
+++ tal.sh
@@ -1,9 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 ROOT=$(dirname "$0")
 
 export FFI_ROOTS="$ROOT/deps/luajit/src;$ROOT/deps/libev/bin/Linux;$FFI_ROOTS"
 export LUA_ROOTS="$ROOT/deps/luajit/src;$LUA_ROOTS"
-
 # export LUA_PATH="$ROOT/lib/?.lua;$ROOT/lib/?/init.lua;$LUA_PATH;;"
-exec "$ROOT/deps/luajit/src/luajit" "$ROOT/lib/tal.lua" "$@"
+
+if [ "$1" = "--gdb" ]; then
+	shift 1
+	exec gdb --args "$ROOT/deps/luajit/src/luajit" "$ROOT/lib/tal.lua" "$@"
+else
+	exec "$ROOT/deps/luajit/src/luajit" "$ROOT/lib/tal.lua" "$@"
+fi
+
