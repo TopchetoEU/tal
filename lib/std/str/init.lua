d57c09adf1022754322664611b3b8778808867bf
TopchetoEU <36534413+TopchetoEU@users.noreply.github.com>
2026-09-04 14:18:32 +0300

    first work on streams

---
+++ lib/std/str/init.lua
@@ -0,0 +1,41 @@
+--- @alias str std.str Nice alias for lazy ppl
+
+--- @class std.str
+local str = {};
+
+--- @param ptr ffi.cdata*
+--- @param n integer
+--- @return integer? n If nil, indicates an EOF
+function str:rawread(ptr, n) ierror "not supported" end
+--- @param ptr ffi.cdata*
+--- @param n integer
+--- @return integer n
+function str:rawwrite(ptr, n) ierror "not supported" end
+--- @param whence "set" | "cur" | "end"
+--- @param pos integer
+--- @return integer
+function str:seek(whence, pos) ierror "not supported" end
+--- @return true _ Must be returned, so that `assert(self:flush())` doesn't error out
+function str:flush() return true end
+--- @return std.io.stat
+function str:stat() ierror "not supported" end
+--- @param mode integer
+function str:rawchmod(mode) ierror "not supported" end
+--- @param mode integer
+function str:rawchown(mode) ierror "not supported" end
+--- @return true _ Must be returned, so that `assert(self:flush())` doesn't error out
+function str:close() return true end
+
+--- @param mode integer | string
+function str:chmod(mode)
+	if type(mode) == "string" then mode = assert(tonumber(mode, 8), "bad mode") end
+	self:rawchmod(mode);
+	return self;
+end
+--- @param mode integer | string
+function str:chown(mode)
+	if type(mode) == "string" then mode = assert(tonumber(mode, 8), "bad mode") end
+	self:rawchown(mode);
+	return self;
+end
+
