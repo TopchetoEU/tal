1b096d3f51b48cdbb1bbff490f52668e12ca53bb
TopchetoEU <me@topcheto.eu>
2026-09-05 18:27:47 +0300

    simplify ssl pipe and adopt new changes

--- lib/std/pipes/ssl.lua
+++ lib/std/pipes/ssl.lua
@@ -1,9 +1,32 @@
 local ffi = require "ffi";
 local libssl = require "nat.libssl";
 local cond = require "std.sync.cond";
-local str  = require "std.str"
+local str = require "std.str";
 
-local function ssl_doread(self)
+--- @class std.io.ssl_opts
+--- @field backend std.str The stream over which to do TLS
+--- @field host? string For clients, name of the server we are connecting to
+--- @field owned? boolean If set to true, closing the created stream will close the backend too
+--- @field role? "client" | "server" What role the TLS stream will have. By default client
+--- @field cert? string Certificate to use. Only for servers
+--- @field key? string Private key to use. Only for servers
+
+--- @class std.pipes.ssl: std.str
+--- @field hnd nat.libssl.ssl
+--- @field bin nat.libssl.bio
+--- @field bout nat.libssl.bio
+---
+--- @field str std.str
+--- @field owned boolean
+---
+--- @field reading boolean
+--- @field writting boolean
+--- @field cond std.sync.cond
+local ssl_str = {};
+ssl_str.__index = ssl_str;
+ssl_str.__metatable = "std.pipes.ssl";
+
+local function _doread(self)
 	if self.reading then
 		-- A bit shitty, makes sure that we block on the current read, as if we did it
 		self.cond:wait();
@@ -14,7 +37,7 @@ local function ssl_doread(self)
 
 	local ptr = ffi.new "char[8192]";
 
-	local ok, n, trace = spcall(self.stream.read, self.stream, ptr, 8192);
+	local ok, n, trace = spcall(self.str.read, self.str, ptr, 8192);
 	if n == 0 then ok = false; n = "unexpected ssl stream eof" end
 	if not ok then
 		self.reading = false;
@@ -29,7 +52,7 @@ local function ssl_doread(self)
 	self.cond:signal(true);
 	return true;
 end
-local function ssl_dowrite(self)
+local function _dowrite(self)
 	if self.writting then
 		self.cond:wait();
 		return false;
@@ -40,12 +63,12 @@ local function ssl_dowrite(self)
 	local buff = ffi.new "char[8192]";
 
 	while true do
-		if not self.stream then ierror "closed" end
+		if not self.str then ierror "closed" end
 
 		local n = self.bout:read(8192, buff);
 		if not n or n == 0 then break end
 
-		local ok, err, trace = spcall(self.stream.fullwrite, self.stream, buff, n);
+		local ok, err, trace = spcall(self.str.fullwrite, self.str, buff, n);
 		if not ok then
 			self.writting = false;
 			self.cond:signal(true);
@@ -58,28 +81,62 @@ local function ssl_dowrite(self)
 	return true;
 end
 
-local function ssl_flush(self)
+function ssl_str:_read(ptr, n)
 	if not self.hnd then ierror "closed" end
-	ssl_dowrite(self);
+
+	while true do
+		local curr_n, code = self.hnd:read(n, ptr);
+		if curr_n and not code then return curr_n end
+
+		local err_code = self.hnd:get_error(0);
+
+		if err_code == 6 then
+			ierror "pipe broken";
+		elseif err_code == 5 then
+			return 0;
+		elseif err_code == 2 then
+			if _dowrite(self) then _doread(self) end
+		elseif err_code ~= 3 then
+			ierror(libssl.err_msg(code));
+		end
+	end
 end
-local function ssl_close(self)
-	if not self.stream then return end
-	if self.owned then self.stream:close() end
+function ssl_str:_write(ptr, n)
+	if not self.hnd then ierror "closed" end
+
+	while true do
+		local res_n, code = self.hnd:write(n, ptr);
+		if res_n then
+			_dowrite(self);
+			return res_n;
+		end
+
+		local err_code = self.hnd:get_error(0);
+
+		if err_code == 6 then
+			ierror "pipe broken";
+		elseif err_code == 5 then
+			return 0;
+		elseif err_code == 2 then
+			if _dowrite(self) then _doread(self) end
+		elseif err_code ~= 3 then
+			ierror(libssl.err_msg(code));
+		end
+	end
+end
+function ssl_str:_flush()
+	if not self.hnd then ierror "closed" end
+	_dowrite(self);
+end
+function ssl_str:_close()
+	if not self.str then return end
+	if self.owned then self.str:close() end
 	self.cond:signal(true);
-	self.stream = nil;
+	self.str = nil;
 end
 
---- @class std.io.ssl_opts
---- @field backend std.str The stream over which to do TLS
---- @field host? string For clients, name of the server we are connecting to
---- @field owned? boolean If set to true, closing the created stream will close the backend too
---- @field role? "client" | "server" What role the TLS stream will have. By default client
---- @field cert? string Certificate to use. Only for servers
---- @field key? string Private key to use. Only for servers
-
---- Returns a TLS stream, using `backend` as the transport
 --- @param opts std.io.ssl_opts
-return function (opts)
+function ssl_str.new(opts)
 	local owned = opts.owned or false;
 	local role = opts.role or "client";
 	local backend = opts.backend;
@@ -101,74 +158,19 @@ return function (opts)
 		hnd:set_accept_state();
 	end
 
-	local self = setmetatable({
+	return setmetatable({
 		hnd = hnd,
 		stream = backend,
 
 		reading = false,
 		writting = false,
 		cond = cond.new(),
-		-- readlock = mutex.new(),
-		-- writelock = mutex.new(),
 
 		bin = bin,
 		bout = bout,
 
 		owned = owned,
-	}, str);
-
-	function self:read(ptr, n)
-		if not self.hnd then ierror "closed" end
-
-		while true do
-			local curr_n, code = self.hnd:read(n, ptr);
-			if curr_n and not code then return curr_n end
-
-			local err_code = self.hnd:get_error(0);
-
-			if err_code == 6 then
-				ierror "pipe broken";
-			elseif err_code == 5 then
-				return 0;
-			elseif err_code == 2 then
-				if ssl_dowrite(self) then ssl_doread(self) end
-			elseif err_code ~= 3 then
-				ierror(libssl.err_msg(code));
-			end
-		end
-	end
-	function self:write(ptr, n)
-		if not self.hnd then ierror "closed" end
-
-		while true do
-			local res_n, code = self.hnd:write(n, ptr);
-			if res_n then
-				ssl_dowrite(self);
-				return res_n;
-			end
-
-			local err_code = self.hnd:get_error(0);
-
-			if err_code == 6 then
-				ierror "pipe broken";
-			elseif err_code == 5 then
-				return 0;
-			elseif err_code == 2 then
-				if ssl_dowrite(self) then ssl_doread(self) end
-			elseif err_code ~= 3 then
-				ierror(libssl.err_msg(code));
-			end
-		end
-	end
-	function self:flush()
-		if not self.hnd then ierror "closed" end
-
-		ssl_flush(self);
-		self.stream:flush();
-	end
-	function self:close()
-		ssl_close(self);
-	end
-
-	return self;
+	}, ssl_str);
 end
+
+return ssl_str;
