acb94ae6ced359bcab04879622bd8b502fe870ec
TopchetoEU <me@topcheto.eu>
2026-09-22 04:53:59 +0300

    rework zlib example

--- examples/zlib.lua
+++ examples/zlib.lua
@@ -9,6 +9,10 @@ local function compress(data, level)
 	return dst.data:tostring();
 end
 return function (fname)
+	if fname == "--help" then
+		print "Compresses a file with each zlib level and outputs the result size and coefficient";
+		print "examples.zlib <file name>";
+	end
 	local data;
 	if fname == "-" then
 		data = io.stdin:read "a";
@@ -16,7 +20,10 @@ return function (fname)
 		data = io.open(fname):read "a";
 	end
 
+	print "LVL %      IN           OUT";
+
 	for level = 0, 9 do
-		print("LVL", level, "IN", #data, "OUT", #compress(data, level));
+		local res = #compress(data, level);
+		print(("%d   %5.1f%% %-12d %-12d"):format(level, res * 100. / tonumber(#data), #data, res));
 	end
 end
